d0cd5e0bf1da31403a15ae3cf60ee896425227e6
ZhenyuChen668 <hnhdxx10451@gmail.com>
2026-02-16 14:37:21 -0500

    Delete BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++
@@ -1,9 +0,0 @@
-def getBondPrice_Z(face, couponRate, times, yc):
-    C = face * couponRate
-    bondPrice = 0.0
-    for t, y in zip(times, yc):
-        cf = C
-        if t == times[-1]:
-            cf = cf + face
-        bondPrice += cf / (1 + y) ** t
-    return bondPrice
