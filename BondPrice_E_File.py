091782ef9de5eb065ea751879f224e545e9baa83
ZhenyuChen668 <hnhdxx10451@gmail.com>
2026-02-16 14:37:02 -0500

    Delete BondPrice_E_File.py

--- BondPrice_E_File.py
+++
@@ -1,9 +0,0 @@
-def getBondPrice_E(face, couponRate, m, yc):
-    C = face * couponRate    
-    bondPrice = 0.0
-    for t, y in enumerate(yc[:m], start=1):
-        cf = C
-        if t == m:
-            cf += face       
-        bondPrice += cf / (1 + y) ** t
-    return bondPrice
