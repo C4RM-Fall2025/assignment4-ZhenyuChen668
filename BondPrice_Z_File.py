b49024cd418b22ad7f66019531be5efda7719ba6
ZhenyuChen668 <hnhdxx10451@gmail.com>
2026-02-16 14:19:22 -0500

    BondPrice_Z_File.py

---
+++ BondPrice_Z_File.py
@@ -0,0 +1,9 @@
+def getBondPrice_Z(face, couponRate, times, yc):
+    C = face * couponRate
+    bondPrice = 0.0
+    for t, y in zip(times, yc):
+        cf = C
+        if t == times[-1]:
+            cf = cf + face
+        bondPrice += cf / (1 + y) ** t
+    return bondPrice
