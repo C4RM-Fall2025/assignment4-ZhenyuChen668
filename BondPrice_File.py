bdffe38be0605cc5c2f65fa8651d3dd640fa450b
ZhenyuChen668 <hnhdxx10451@gmail.com>
2026-02-16 14:45:56 -0500

    practice

---
+++ BondPrice_File.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    r = y / ppy
+    N = m * ppy
+    C = face * couponRate / ppy
+    bondPrice = C * (1 - (1 + r) ** (-N)) / r + face / (1 + r) ** N
+    return(bondPrice)
+=======
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    r = y / ppy
+    N = m * ppy
+    C = face * couponRate / ppy
+    bondPrice = C * (1 - (1 + r) ** (-N)) / r + face / (1 + r) ** N
+    return(bondPrice)
+
+>>>>>>> 09ebeede1359a071488093f1fc59a4ebebb067a3
